2d9a5f285c7fe7e94617b184da8c0c78b3cbd3ec
Dayoung <dy.kim@datadriven.kr>
2021-04-26 03:03:31 +0900

    feat(clinet): 커버페이지 틀잡기

--- src/pages/Cover/index.tsx
+++ src/pages/Cover/index.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import IconButton from '@material-ui/core/IconButton';
+
+const WholeDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const CenterDiv = styled.div`
+  background-color: pink;
+  width: 90%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CircleButton = styled.button`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  &:hover {
+    box-shadow: 5px 5px 3px lightgray;
+  }
+`;
+const Arrow = styled(IconButton)`
+  .MuiSvgIcon-root {
+    font-size: 5rem;
+  }
+`;
 
 const Cover: React.FC = () => {
   const history = useHistory();
   return (
-    <div>
-      <button type="button" onClick={() => history.push('/home')}>
-        입장
-      </button>
-    </div>
+    <WholeDiv>
+      <Arrow>
+        <ArrowLeftIcon />
+      </Arrow>
+      <CenterDiv>
+        <CircleButton onClick={() => history.push('/home')}>
+          ENTRANCE
+        </CircleButton>
+      </CenterDiv>
+      <Arrow>
+        <ArrowRightIcon />
+      </Arrow>
+    </WholeDiv>
   );
 };
 
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -9,11 +9,27 @@ const GlobalStyle = createGlobalStyle`
     overflow: hidden;
     padding: 0;
     margin: 0;
-    font-size: 18px;
+    font-size: 16px;
   }
 
   * {
     box-sizing: border-box;
+  }
+
+  body,
+  body *,
+  input,
+  textarea,
+  ::selection,
+  button {
+  font-family: inherit;
+  letter-spacing: -0.12px;
+  }
+
+  button {
+    outline: 0;
+    border: 0;
+    cursor: pointer;
   }
 `;
 
